a2e868c7bc909b817b11c5dab059d8235a3d908d
Tony Schmich <tonyschmich@gmail.com>
2023-03-27 20:08:06 +0200

    fix: project setup

--- Source/GFT_Spring23/GFT_Spring23.Build.cs
+++ Source/GFT_Spring23/GFT_Spring23.Build.cs
@@ -8,7 +8,7 @@ public class GFT_Spring23 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "GFT_Spring23" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
