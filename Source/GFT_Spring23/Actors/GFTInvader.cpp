ba479c111a954def9ccf5f2bf05e116ecf9dc946
Tony Schmich <tonyschmich@gmail.com>
2023-03-29 09:05:56 +0200

    feat: added GFTInvader feat: added invaders to map

---
+++ Source/GFT_Spring23/Actors/GFTInvader.cpp
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GFTInvader.h"
+
+#include "Components/BoxComponent.h"
+
+AGFTInvader::AGFTInvader()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	Collision = CreateDefaultSubobject<UBoxComponent>(TEXT("Collision"));
+	Collision->SetCollisionProfileName(TEXT("BlockAll"));
+	Collision->SetEnableGravity(false);
+	RootComponent = Collision;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	Mesh->SetupAttachment(Collision);
+	Mesh->SetCollisionProfileName(TEXT("NoCollision"));
+	Mesh->SetGenerateOverlapEvents(false);
+}
---
+++ Source/GFT_Spring23/Actors/GFTInvader.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "GFTInvader.generated.h"
+
+class UBoxComponent;
+
+UCLASS()
+class GFT_SPRING23_API AGFTInvader : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AGFTInvader();
+
+private:
+	// Box used as RootComponent and for collision
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=GFT, meta=(AllowPrivateAccess = "true"))
+	TObjectPtr<UBoxComponent> Collision;
+
+	// Mesh with disabled collision
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=GFT, meta=(AllowPrivateAccess = "true"))
+	TObjectPtr<UStaticMeshComponent> Mesh;
+};
